9067fa89eb2a7d2add7670e60d1edc13151ea6a7
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-01-09 17:33:30 +0530

    Create task4.cpp
    Manipulators

---
+++ task4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<ostream>
+#include<istream>
+#include<string>
+#include<sstream>
+using namespace std;
+int main ()
+{
+    cout<<"ABC, ";
+    cout<<"EFG"<<endl;
+    cout<<"HIJ"<<ends;
+    cout<<"KLM"<<flush;
+    
+}
