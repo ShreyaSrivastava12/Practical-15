6e95ed9fccb867f9cbe820298c4fb68503cc636f
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-01-09 17:35:00 +0530

    Create task5.cpp
    Manipulators

---
+++ task5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<ostream>
+#include<istream>
+#include<string>
+#include<sstream>
+#include<iomanip>
+using namespace std;
+int main ()
+{
+    double f=3.14159;
+    cout<<setprecision(5)<<f<<'\n';
+    cout<<setw(8)<<21<<endl;
+}
